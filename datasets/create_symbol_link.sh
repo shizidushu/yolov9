cd13929e3076057f9a62c3cc0c8b4ef8dbb1f34c
shizidushu <shizidushu@gmail.com>
2024-03-23 16:17:51 +0800

    start training

---
+++ datasets/create_symbol_link.sh
@@ -0,0 +1,9 @@
+# ln -s /mnt/d/0-Datasets/public/COCO2017/annotations coco/annotations
+# ln -s /mnt/d/0-Datasets/public/COCO2017/images coco/images
+# ln -s /mnt/d/0-Datasets/public/COCO2017/labels-yolov9 coco/labels
+# ln -s /mnt/d/0-Datasets/public/COCO2017/train2017.txt coco/train2017.txt
+# ln -s /mnt/d/0-Datasets/public/COCO2017/val2017.txt coco/val2017.txt
+# cp /mnt/d/0-Datasets/public/COCO2017/train2017.txt train2017.txt
+# cp /mnt/d/0-Datasets/public/COCO2017/val2017.txt val2017.txt
+
+ln -s /mnt/d/0-Datasets/public/coco2017-yolov9 coco
---
+++ training_note.md
@@ -0,0 +1,6 @@
+
+
+修改data/coco.yaml中的path:为`datasets/coco`
+```bash
+python train_dual.py --workers 8 --device 0 --batch 16 --data data/coco.yaml --img 640 --cfg models/detect/yolov9-c.yaml --weights '' --name yolov9-c --hyp hyp.scratch-high.yaml --min-items 0 --epochs 500 --close-mosaic 15
+```
